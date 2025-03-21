6c1fa45e86b54a58f8b47e2d6d7216278e743ab5
Alberto <chromealberto@gmail.com>
2025-03-21 23:47:02 +0100

    Idiomas

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.hilt) apply false
+    id("kotlin-kapt")
+    alias(libs.plugins.android.hilt)
 }
